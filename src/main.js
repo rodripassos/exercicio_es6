cdbf332506a0a6b15bea30bfaba0ba2ba481843f
Rodrigo Passos <rodrigopassosnascimento@gmail.com>
2024-01-13 13:57:52 -0300

    correção exercicio

--- src/main.js
+++ src/main.js
@@ -1,33 +1,20 @@
-const alunos = [['Gustavo', 5], ['Maria', 3], ['João', 9], ['Paula', 6], ['Francisco', 10]];
+const alunos = [{nome:'Gustavo', nota: 5},
+                {nome:'Maria', nota: 3}, 
+                {nome:'João', nota: 9}, 
+                {nome: 'Paula', nota: 6}, 
+                {nome: 'Francisco', nota: 10}];
 
-const alunos2 = alunos.map(function(itemAtual) {
-    
-    itemAtual = {
-        nome: itemAtual[0],
-        nota: itemAtual[1]
-    }
-    return itemAtual;
-})
-
-class Aprovado {
-    constructor(nomeDoAluno, notaDoAluno) {
-        this.nome = nomeDoAluno;
-        this.nota = notaDoAluno;
-    }
-
-    passou() {
-        if (this.nota >= 6) { 
-        console.log(`O(a) aluno(a) ${this.nome} foi aprovado(a) com a nota: ${this.nota}`);
-        }
-    }
-        
+                
+function filtraAlunosAprovados(aluno) {
+    return aluno.nota >= 6;
 }
 
-for (let resultado of alunos2.values()) {
+const alunosAprovados = alunos.filter(filtraAlunosAprovados)
+
+for (let resultado of alunosAprovados.values()) {
     let nome = resultado.nome;
     let nota = resultado.nota;
-    let aprovado = new Aprovado(nome, nota)
-    aprovado.passou();
+    console.log(`O(a) aluno(a) ${nome} foi aprovado(a) com a nota: ${nota}`);
 }
 
 
